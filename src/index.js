9da164675b30a1e709a3b85339adf8239183c99b
yoon <parkyoon97@gmail.com>
2023-03-30 04:54:35 +0000

    image file upload

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
+<h1>서버는 1234 포트번호 위에서만 적용이 된다</h1>
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
   info about Parcel 
